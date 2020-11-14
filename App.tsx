cde7b9f5a1f91f3ab8910f6f4517c1cc3ffd6a54
Ryan Buckley <ryanbuckley@gmail.com>
2020-11-13 20:16:09 -0500

    add env to gitignore

--- App.tsx
+++ App.tsx
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AIRTABLE_ID } from '@env';
 
-export default function App() {
+function App() {
+  const [ data, setData ] = useState();
+  
+  const airTableURL = 'https://api.airtable.com/v0/appA67zYW50gE6q8E/BUDGET'
+  const APIpost = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${AIRTABLE_ID}`
+    },
+    data: {
+      "records": [
+        {
+          "fields": {}
+        },
+        {
+          "fields": {}
+        }
+      ]
+    }
+  }
+  const APIget = {
+    method: 'GET',
+    headers: { Authorization: `Bearer ${AIRTABLE_ID}` },
+  }
+
+
+  useEffect(() => {
+
+    fetch(airTableURL, APIget)
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{'state is ', data}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +54,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
