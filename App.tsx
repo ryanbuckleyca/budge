e2fd9fd7ea5497b13f12c6ccec4b98e51dfb2778
Ryan Buckley <ryanbuckley@gmail.com>
2020-11-14 02:43:39 -0500

    clear form on submit, update state

--- App.tsx
+++ App.tsx
@@ -1,76 +1,46 @@
 import React, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { getLogRecords, postLogRecord } from './scripts/airtable.js'
+import { getLogRecords, postLogRecord } from './scripts/airtable.tsx'
+// components
+import ListLogs from './components/list-logs.tsx'
+import EntryForm from './components/log-entry-form.tsx'
+import styles from './styles.tsx'
 
 function App() {
-  const [ data, setData ] = useState([]);
-  const [ entry, setEntry ] = useState({});
+  const blankEntry = {
+    Type: 'Expense', Amount: '', Description: '', Category: '', Notes: ''
+  }
+  const [ logs, setLogs ] = useState();
+  const [ entry, setEntry ] = useState(blankEntry);
 
   useEffect(() => {
     getLogRecords()
-    .then(data => setData(data.records))
-    .catch(err => console.log('fetch error: ', err.message))
+      .then(data => setLogs(data.records))
+      .catch(err => console.log('fetch error: ', err.message))
   }, [])
 
   const handleChange = (e) => {
     const { name, value } = e.target
     entry[name] = name === 'Amount' ? parseFloat(value) : value
-    console.log('entry is now ', entry)
+    setEntry({...entry})
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // TODO: perform checks before POSTing
     postLogRecord(entry)
-    .then(data => console.log(data))
-
+      .then(res => setLogs([...logs, res]))
+      .catch(err => console.log('error in posting: ', err))
+      .finally(console.log('submitted: state of logs is ', logs))
+    setEntry(blankEntry)
   }
-
-  const printLogs = data.map(record => {
-    const { Type, Amount, Description, Category, Notes } = record.fields
-    return (
-      <tr key={record.id} style={styles.textLeft}>
-        <td>
-          <h2 style={{ color: Type === 'Expense' ? 'red' : 'green' }}>
-            { Amount }
-          </h2>
-        </td>
-        <td><strong>{ Description }</strong></td>
-        <td>{ Category }</td>
-        <td>{ Notes }</td>
-      </tr>
-    )
-  })
-
-  const newEntry =
-    <tr style={styles.textLeft}>
-      <td>
-        <input type="number" name="Amount" value={entry.Amount} onChange={(e) => handleChange(e)} />
-        <span>
-          <input type="radio" name="Type" value="Expense" selected={entry.Type==='Expense'} onChange={(e) => handleChange(e)} />
-          Expense
-          <input type="radio" name="Type" value="Income" selected={entry.Type==='Income'} onChange={(e) => handleChange(e)} />
-          Income
-        </span>
-      </td>
-      <td>
-        <input type="text" name="Description" value={ entry.Description } onChange={(e) => handleChange(e)} />
-      </td>
-      <td>
-        <input type="text" name="Category" value={ entry.Category } onChange={(e) => handleChange(e)} />
-      </td>
-      <td>
-        <span style={styles.nowrap}>
-        <input type="text" name="Notes" value={ entry.Notes } onChange={(e) => handleChange(e)} />
-        <button onClick={(e) => handleSubmit(e)}>+</button>
-        </span>
-      </td>
-    </tr>
 
   return (
     <View style={ styles.container }>
-      <Text style={styles.h}>{ `${ data.length } items in LOG:` }</Text>
+      <Text style={styles.h}>
+        { `${ logs && logs.length || 'no' } items in LOG:` }
+      </Text>
       <View style={styles.w100}>
         <table style={styles.w100}>
           <thead style={styles.textLeft}>
@@ -82,33 +52,17 @@ function App() {
             </tr>
           </thead>
           <tbody>
-          { newEntry }
-          { printLogs }
+          <EntryForm
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            entry={entry} />
+          {logs && (logs.length > 0) && <ListLogs logs={logs} />}
           </tbody>
         </table>
       </View>
       <StatusBar style="auto" />
     </View>
   );
-}
-
-const styles = {
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: '3em'
-  },
-  w100: {
-    width: '100%',
-  },
-  textLeft: {
-    textAlign: 'left',
-  },
-  nowrap: {
-    whiteSpace: 'nowrap'
-  }
 }
 
 export default App;
---
+++ styles.tsx
@@ -0,0 +1,20 @@
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '3em'
+  },
+  w100: {
+    width: '100%',
+  },
+  textLeft: {
+    textAlign: 'left',
+  },
+  nowrap: {
+    whiteSpace: 'nowrap'
+  }
+}
+
+export default styles;
